70d0c27834fb98fb7d518389e59232a13df01691
roscoe9008 <77300179+roscoe9008@users.noreply.github.com>
2021-04-18 14:59:20 -0400

    Add files via upload

---
+++ finalproject/js/functions.js
@@ -0,0 +1,9 @@
+	function open_google(){
+		window.location='https://google.com';
+	}
+	function open_yahoo(){
+		window.location='https://yahoo.com';
+	}
+	function open_bing(){
+		window.location='https://bing.com';
+	}
